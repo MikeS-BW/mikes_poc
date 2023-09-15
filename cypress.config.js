8e326bc1ca927bcc50da753c65720c0ca86164f2
Michael Salamida <michael.salamida@businesswire.com>
2023-09-15 14:39:57 -0700

    adding project alpha

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "eo4s4h",
+  projectId: "4jt5in",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
