e7c83bad774b2d13023da192cd80ec5c1e585da3
Michael Salamida <michael.salamida@businesswire.com>
2023-09-20 13:39:53 -0700

    adding project beta

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "eo4s4h",
   projectId: "4jt5in",
+  projectId: "rxrecv",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
